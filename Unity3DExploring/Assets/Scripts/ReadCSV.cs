7a384550f5b31480d17b5394d4c44cca8fce7001
S3943755 <s3943755@student.rmit.edu.au>
2024-10-09 08:21:30 +1100

    add csv of people locations and made people move
    - read csv to store positions and facing direction in a dictionary of dictionaries stored as: - Name: (Frame: (x_pos, direction))
    - cleaned csv as data was stored incorrectly (as a temporary solution TO FIX)

---
+++ Unity3DExploring/Assets/Scripts/ReadCSV.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ReadCSV : MonoBehaviour
+{
+    // Public property to access poseData
+    public Dictionary<string, Dictionary<int, (float xPos, float rotation)>> PoseData { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoseData = new Dictionary<string, Dictionary<int, (float, float)>>(); // Initialize the dictionary
+        ReadCSVFile();
+    }
+
+    void ReadCSVFile()
+    {
+        StreamReader strReader = new StreamReader("Assets/Locations/pose_estimation_data.csv");
+        bool endOfFile = false;
+
+        strReader.ReadLine(); // Skip the header line
+
+        while (!endOfFile)
+        {
+            string dataString = strReader.ReadLine();
+            if (dataString == null)
+            {
+                endOfFile = true;
+                break;
+            }
+
+            var dataValues = dataString.Split(',');
+
+            int frame = int.Parse(dataValues[0]);
+            string objectName = dataValues[1];
+            float xPos = float.Parse(dataValues[2]) / 100;
+            float rotation = float.Parse(dataValues[3]);
+
+            if (!PoseData.ContainsKey(objectName))
+            {
+                PoseData[objectName] = new Dictionary<int, (float, float)>();
+            }
+
+            PoseData[objectName][frame] = (xPos, rotation);
+        }
+    }
+
+    // Function to return only the data for a specific object by name
+    public Dictionary<int, (float xPos, float rotation)> GetObject(string objectName)
+    {
+        // Check if the object exists in the PoseData dictionary
+        if (PoseData.ContainsKey(objectName))
+        {
+            return PoseData[objectName];
+        }
+        else
+        {
+            Debug.LogError($"Object '{objectName}' not found in PoseData.");
+            return null; // Return null if the object is not found
+        }
+    }
+}
