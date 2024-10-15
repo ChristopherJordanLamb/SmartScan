72fe3f8dfe8546332e995b2d1d2a7c2abf29cea1
S3943755 <s3943755@student.rmit.edu.au>
2024-10-15 16:36:40 +1100

    use updated automated csv for movement
    - ReadCSVMoreInfo stores zPos for future implementation once zPos is solved
    - fix sample scene to be the video demo scene
    - TODO: update targetPos to be once a position is reached (comparing currPosition to targetPos)
    - frame time decreased from 0.5 -> 0.1 to speed up the time to read a new frame (temporary solution)

--- Unity3DExploring/Assets/Scripts/ReadCSVMoreInfo.cs
+++ Unity3DExploring/Assets/Scripts/ReadCSVMoreInfo.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class ReadCSV : MonoBehaviour
+public class ReadCSVMoreInfo : MonoBehaviour
 {
     // Public property to access poseData
-    public Dictionary<string, Dictionary<int, (float xPos, float rotation)>> PoseData { get; private set; }
+    public Dictionary<string, Dictionary<int, (float xPos, float zPos, float rotation)>> PoseData { get; private set; } // added zPos (for future when zPos is fixed)
 
     public string csvFileName;
 
     // Start is called before the first frame update
     void Start()
     {
-        PoseData = new Dictionary<string, Dictionary<int, (float, float)>>(); // Initialize the dictionary
+        PoseData = new Dictionary<string, Dictionary<int, (float, float, float)>>(); // Initialize the dictionary
         ReadCSVFile();
     }
 
@@ -38,19 +38,20 @@ public class ReadCSV : MonoBehaviour
             int frame = int.Parse(dataValues[0]);
             string objectName = dataValues[1];
             float xPos = float.Parse(dataValues[2]) / 100;
-            float rotation = float.Parse(dataValues[3]);
+            float zPos = float.Parse(dataValues[3]) / 100;
+            float rotation = float.Parse(dataValues[4]);
 
             if (!PoseData.ContainsKey(objectName))
             {
-                PoseData[objectName] = new Dictionary<int, (float, float)>();
+                PoseData[objectName] = new Dictionary<int, (float, float, float)>();
             }
 
-            PoseData[objectName][frame] = (xPos, rotation);
+            PoseData[objectName][frame] = (xPos, zPos, rotation);
         }
     }
 
     // Function to return only the data for a specific object by name
-    public Dictionary<int, (float xPos, float rotation)> GetObject(string objectName)
+    public Dictionary<int, (float xPos, float zPos, float rotation)> GetObject(string objectName)
     {
         // Check if the object exists in the PoseData dictionary
         if (PoseData.ContainsKey(objectName))
